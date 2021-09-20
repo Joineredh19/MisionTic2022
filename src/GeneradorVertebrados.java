33c657b38cd11517098efaec469bea6818ed051e
Joiner2003 <joineredh@gmail.com>
2021-09-20 07:20:31 -0500

    Se hizo el cambio de pez por pescado

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -3,7 +3,7 @@ public class GeneradorVertebrados {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Perro", "Pez");
+        miVertebrados[2] = new Vertebrado("Perro", "Pescado");
 
         return miVertebrados;
     }
